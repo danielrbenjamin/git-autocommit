4d58b384d6a14352adc4c625a3df26474479d590
Daniel Benjamin <156981781+danielrbenjamin@users.noreply.github.com>
2024-08-13 01:56:38 -0700

    Update autoGitCommit.ps1

--- autoGitCommit.ps1
+++ autoGitCommit.ps1
@@ -1,13 +1,13 @@
 # Check for pause file
-if (Test-Path -Path `"$projectDir\New Text Document.txt`") {
+if (Test-Path -Path "PROJECTDIRECTORY") {
     exit
 }
 
 # Navigate to the project directory
-cd `"$projectDir`"
+cd "PROJECTDIRECTORY"
 
 # Add all changes
 git add -A
 
 # Commit added changes with a timestamp message
-git commit -m `"Auto-commit: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`"
+git commit -m "Auto-commit: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
