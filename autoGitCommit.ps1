843265bde72b2911a7bcc758eacbfdde8ead1d56
Daniel Benjamin <156981781+danielrbenjamin@users.noreply.github.com>
2024-10-13 16:54:08 -0700

    Update autoGitCommit.ps1

--- autoGitCommit.ps1
+++ autoGitCommit.ps1
@@ -1,25 +1,13 @@
-# Define project directory as the current location
-$projectDirectory = Get-Location
-
-# Check if this is a git repository (i.e., if .git folder exists)
-if (-not (Test-Path "$projectDirectory\.git")) {
-    Write-Host "This directory is not a git repository."
-    exit
-}
-
 # Check for pause file
-if (Test-Path -Path "$projectDirectory\.autogitpause") {
-    Write-Host "Autogit is paused. Exiting..."
+if (Test-Path -Path "PROJECTDIRECTORY\New Text Document.txt") {
     exit
 }
 
-# Get the list of changed files along with their status
-$changedFiles = git status --porcelain
+# Navigate to the project directory
+cd "PROJECTDIRECTORY"
 
 # Add all changes
 git add -A
 
-# Commit added changes with a timestamp and list of changed files with their status
-$commitMessage = "Auto-commit: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`nFiles changed:`n$($changedFiles -join "`n")"
-
-git commit -m "$commitMessage"
+# Commit added changes with a timestamp message
+git commit -m "Auto-commit: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
