7aed98ae4e9eee08e4296cae6a4271682a969f28
Daniel Benjamin <156981781+danielrbenjamin@users.noreply.github.com>
2024-10-13 17:45:13 -0700

    Update autoGitCommit.ps1

--- autoGitCommit.ps1
+++ autoGitCommit.ps1
@@ -1,8 +1,19 @@
+$projectDirectory = Get-Location
+
+# Check for pause file
+if (Test-Path -Path "$projectDirectory\.autogitpause") { 
+    exit
+}
+
 # Add all changes
 git add -A
 
 # Get the list of changed files along with their status
-$changedFiles = git status --porcelain
+$changedFilesArray = git diff --cached --name-status
 
-# Commit added changes with a timestamp message
-git commit -m "Auto-commit: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`nFiles changed:`n$(${changedFiles} -join "`n")"
+$changedFiles = $changedFilesArray -join "`n"
+
+# Commit added changes with a timestamp in the title and the list of changed files with their status in the description
+$commitTitle = "Auto-commit: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+$commitDescription = "Files changed:`n$($changedFiles)"
+git commit -m "$commitTitle" -m "$commitDescription"
